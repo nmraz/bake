c83a43c5482b07ffab8e2a0270f55aa1bd3914ea
Noam Raz <noamraz8@gmail.com>
2025-11-21 01:12:34 +0200

    Set up much simpler build system
    It is still KBuild-inspired in certain respects, but is actually pretty
    concise and easy to follow.

--- Makefile
+++ Makefile
@@ -1,15 +1,25 @@
-export srcroot := .
-export objroot := build/obj
+MAKEFLAGS += -rR
 
-ifeq ($(V),1)
-Q :=
-else
-Q := @
-endif
+CC := gcc
 
-export Q
+BUILD_DIR := build
+OBJ_DIR := $(BUILD_DIR)/obj
+BIN_DIR := $(BUILD_DIR)/bin
 
-export CC := gcc
-export AR := ar
+include makefiles/defs.mk
 
-include $(srcroot)/makefiles/defs.mk
+cflags-y += -MMD -MP
+
+.PHONY: all
+all:
+
+quiet_cmd_clean = CLEAN   $(BUILD_DIR)
+      cmd_clean = rm -rf $(BUILD_DIR)
+
+.PHONY: clean
+clean:
+	$(call cmd,clean)
+
+include src/Makefile
+
+include makefiles/build.mk
--- makefiles/defs.mk
+++ makefiles/defs.mk
@@ -1,10 +1,15 @@
-build := -f $(srcroot)/makefiles/build.mk curdir=
+ifeq ($(V),1)
+quiet :=
+else
+quiet := quiet_
+endif
 
-# Useful for describing the dependency of composite objects
-# Usage:
-#   $(call multi_depend, multi_used_targets, suffix_to_remove, suffix_to_add)
-define multi_depend
-$(foreach m, $1, \
-	$(eval $m: \
-	$(addprefix $(obj)/, $(call suffix-search, $(patsubst $(obj)/%,%,$m), $2, $3))))
-endef
+squote  := '
+
+escsq = $(subst $(squote),'\$(squote)',$1)
+
+log-cmd = echo '  $(call escsq,$($(quiet)cmd_$(1)))'
+cmd = @set -e; $(log-cmd); $(cmd_$(1))
+
+cflags-y :=
+bins-y :=
