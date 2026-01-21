01005bc3301af723119481dd80b7a41b9b815017
Noam Raz <noamraz8@gmail.com>
2026-01-21 22:53:17 +0200

    Separate object files for different binaries
    This will allow the same source to be built multiple times, with
    different flags, for different binaries.

--- scripts/build.mk
+++ scripts/build.mk
@@ -1,9 +1,6 @@
 quiet_cmd_cc = CC      $@
       cmd_cc = $(CC) -MMD -MP $(cflags-y) $(cflags-$(linktarget)-y) $(cflags-$<-y) -c $< -o $@
 
-$(OBJ)/%.o: %.c FORCE
-	$(call cmd,cc)
-
 all-ldlibs = $(addprefix -l,$(ldlibs-$(bin-name)-y))
 
 quiet_cmd_ld = LD      $@
@@ -13,11 +10,13 @@ bin-outputs = $(addprefix $(BUILD)/,$(bins-y))
 targets += $(bin-outputs)
 
 define each-bin
-objs-$(bin-name) := $$($(bin-name)-y:%.c=$(OBJ)/%.o) $$(objs-$(bin-name))
+objs-$(bin-name) := $$($(bin-name)-y:%.c=$(OBJ)/$(bin-name)/%.o) $$(objs-$(bin-name))
 
 targets += $$(objs-$(bin-name))
 
 $$(objs-$(bin-name)): private linktarget := $(bin-name)
+$$(objs-$(bin-name)): $(OBJ)/$(bin-name)/%.o: %.c FORCE
+	$$(call cmd,cc)
 
 $(BUILD)/$(bin-name): private bin-name := $(bin-name)
 $(BUILD)/$(bin-name): $$(objs-$(bin-name)) FORCE
